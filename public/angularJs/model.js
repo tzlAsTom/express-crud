e680bc3f200b36a31214b31d16cfa5e36dad5153
tong.zhilian <zhilian.tong@dena.com>
2015-04-24 18:22:08 +0800

    enhance model template

--- public/angularJs/model.js
+++ public/angularJs/model.js
@@ -50,8 +50,23 @@
 
             return rtn;
         };
+
+        //* radiobox/checkbox/select can use remote config
+        /*
+        {
+            "sourceName": [{name: '', value: ''}]
+        }*/
+        this._dataSource = {};
+        this.getDataSource = function(sourceName){
+            return self._dataSource[sourceName] || [];
+        };
+
+        this.setDataSource = function(sourceName, data){
+            if(!Array.isArray(data)) return;
+            self._dataSource[sourceName] = data;
+        };
     }])
-    .controller('main', ['$scope', '$http', function($scope, $http){
+    .controller('main', ['$scope', '$http', '$timeout', function($scope, $http, $timeout){
         $scope.alertMessage = '';
         $scope.isError = false;
 
@@ -62,8 +77,15 @@
             $scope.alertMessage = message;
         };
 
+        var preTimeout;
         $scope.$on('updateMessage', function(e, code, message){
+            $timeout.cancel(preTimeout);
             $scope.updateAlertMessage(code, message);
+            //fade out after timeout
+            preTimeout = $timeout(function(){
+                $scope.updateAlertMessage(code, '');
+                console.log('xxxx');
+            }, 2000);
         });
     }]);
 
@@ -72,17 +94,73 @@
         //{modelName: ""}
         $scope.config = {};
 
-        $scope.query = {};
+
+        var formDataConfig = [
+            {
+                inputName: 'remoteSelect',
+                sourceUrl: '/admin/restful/models/remoteSelect',
+                nameKey: 'name',
+                valueKey: 'number',
+            },
+
+            {
+                inputName: 'remoteRadio',
+                sourceUrl: '/admin/restful/models/remoteRadio',
+                nameKey: 'name',
+                valueKey: 'number',
+            },
+
+            {
+                inputName: 'remoteCheckBox',
+                sourceUrl: '/admin/restful/models/remoteCheckBox',
+                nameKey: 'name',
+                valueKey: 'number',
+            },
+        ];
+
+        $scope.formData = {};
+        formDataConfig.forEach(function(inputDataConfig){
+            Container.getResource(inputDataConfig.sourceUrl).success(function(data){
+                var temp = [];
+                data.forEach(function(elem){
+                    temp.push({name: elem[ inputDataConfig.nameKey ], value: elem[ inputDataConfig.valueKey ]});
+                });
+                Container.setDataSource(inputDataConfig.inputName, temp);
+                $scope.formData[ inputDataConfig.inputName ] = temp;
+            });
+        });
+//         Container.setDataSource('remoteSelect', [
+//             {name: 'option3', value: 3},
+//             {name: 'option4', value: 4},
+//             {name: 'option5', value: 5}
+//         ]);
+//
+//         $scope.formData.remoteSelect = Container.getDataSource('remoteSelect');
+//
+//         Container.setDataSource('remoteRadio', [
+//             {name: 'option13', value: 13},
+//             {name: 'option14', value: 14},
+//             {name: 'option15', value: 15}
+//         ]);
+//         $scope.formData.remoteRadio = Container.getDataSource('remoteRadio');
+//
+//         Container.setDataSource('remoteCheckBox', [
+//             {name: 'option113', value: 113},
+//             {name: 'option114', value: 114},
+//             {name: 'option115', value: 115}
+//         ]);
+//         $scope.formData.remoteCheckBox = Container.getDataSource('remoteCheckBox');
+
+        $scope.curRecord = {};
         $scope.recordList = [];
 
-        $scope.printDebug = function(){
-            console.log($scope.serverSelected, $scope.curRecord, $scope.announcementList);
-        };
+        $scope.query = {};
+
 
         $scope.create = function(){
             var curRecord = angular.copy($scope.curRecord);
             Container.createResoure('/admin/restful/models/' + $scope.config.modelName, curRecord).success(function(data){
-                Container.updateAlertMessage(false, 'Create success');
+                Container.updateAlertMessage(false, 'Create success; _id: ' + data._id);
                 $scope.recordList.unshift(data);
             });
         };
@@ -92,7 +170,7 @@
             var curId = curRecord._id;
             delete curRecord._id;
             Container.updateResource('/admin/restful/models/' + $scope.config.modelName + '/' + curId, curRecord).success(function(data){
-                Container.updateAlertMessage(false, 'Update Success');
+                Container.updateAlertMessage(false, 'Update Success; _id: ' + curId);
                 angular.forEach($scope.recordList, function(elem, index){
                     if(elem._id == curId) $scope.recordList[index] = angular.copy($scope.curRecord);
                 });
@@ -101,7 +179,7 @@
 
         $scope.delete = function(_id){
             Container.deleteResource('/admin/restful/models/' + $scope.config.modelName + '/' + _id).success(function(data){
-                Container.updateAlertMessage(false, 'Delete success');
+                Container.updateAlertMessage(false, 'Delete success; _id: ' + _id);
 
                 angular.forEach($scope.recordList, function(elem, index){
                     if(elem._id == _id) curIndex = index;
@@ -110,6 +188,10 @@
                 if(curIndex != undefined)
                     $scope.recordList.splice(curIndex, 1);
             });
+        };
+
+        $scope.debug = function(){
+            Container.updateAlertMessage(false, JSON.stringify($scope.curRecord, null, '  '));
         };
 
         $scope.reset = function(){
@@ -121,7 +203,6 @@
                 if($scope.query[elem] === '') delete $scope.query[elem];
             });
             Container.getResource('/admin/restful/models/' + $scope.config.modelName, $scope.query).success(function(data){
-                console.log(data);
                 $scope.recordList = data;
             });
         };
